d7f1e7e0dfd49886f3e33cc2ff2c97397a4d1660
Vasyl <film1megogo@gmail.com>
2023-07-15 00:28:30 +0300

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header(props){
 
     return(
         <header className="d-flex justify-between align-center">
-           <Link  to="/simple-shop">
+           <Link  to="/simple-shop/">
         <div className="d-flex align-center">
        
         <img width={100} height={100} src="img/logo.png"/>
